909c5a6990caf6c420f436936c5c01ac0506cf9b
TheArctesian <stephen.d.okita@gmail.com>
2022-06-25 19:09:44 -0700

    first draft

---
+++ Scripts/main.py
@@ -0,0 +1,36 @@
+def parse():
+    print("sd")
+
+def tokenizeHead(array):
+    tempArray = array.replace(" ", "").strip().split(",",)
+    print(tempArray)
+    return tempArray
+
+def tokenizeValues(line):
+    tempArray = line.strip().split(",")
+    return tempArray
+# TODO: Figure out how this works
+def defineArrays(Header):
+    myStr = Header
+    print("The string is:",myStr)
+    myVars = locals()
+    myVars[myStr] = []
+
+
+if __name__ == "__main__":
+    with open("../Test/testData.csv") as f:
+        Headers = f.readline()
+        Headers = tokenizeHead(Headers)
+        ra = len(Headers)
+        print(ra)
+        for word in Headers:
+            globals()[word] = []
+        
+
+        for line in f:
+            a = tokenizeValues(line)
+            print(a)
+            for value in range(ra):
+                globals()[Headers[value]].append(a[value])
+
+        print(globals()[Headers[0]])
--- log.md
+++ log.md
@@ -1,11 +1,23 @@
-<strong>Name: </strong><em>Name or Tag here</em> 
+<strong>Name: </strong><em>Name or Tag here</em>
 <br>
-<strong>Time: </strong>  <em>Time here (24hr time)</em> 
+<strong>Time: </strong> <em>Time here (24hr time)</em>
 <br>
-<strong>Date: </strong>  <em>DD.MM.YY</em> 
+<strong>Date: </strong> <em>DD.MM.YY</em>
 <br>
 <strong>Goal: </strong> <em>Do this before session </em>
 <br>
 <strong>Description: </strong> <em>What happened</em>
+
+<hr>
+
+<strong>Name: </strong>Arctesian
+<br>
+<strong>Time: </strong>18:16-
+<br>
+<strong>Date: </strong> 25.06.22
+<br>
+<strong>Goal: </strong> Make CSV parser
+<br>
+<strong>Description: </strong>
 
 <hr>
