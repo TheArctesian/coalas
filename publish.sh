3afe7400d0fdb843e985d1e50c5396648236dbd8
theArctesian <stephen.d.okita@gmail.com>
2022-07-09 22:15:29 -0700

    0.0.4

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 python3 -m pip install --upgrade build
 python3 -m build
 python3 -m pip install --upgrade twine
-py -m twine upload --repository coalas dist/*
+python3 -m twine upload --repository pypi dist/*
 # this one might return an error, delete dist and do again 
